9b4efd85ed913a5e97423d395397439971e668c1
Borbala Tobias <borbala.tobias@aexp.com>
2021-09-22 15:20:15 +0100

    extract file reader methods into FileReader class

---
+++ src/main/java/training/supportbank/FileReader.java
@@ -0,0 +1,106 @@
+package training.supportbank;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FileReader {
+    private Map<String, Account> accounts;
+    private ArrayList<Transaction> transactions;
+
+    public FileReader(Map<String, Account> initAccounts, ArrayList<Transaction> initTransactions) {
+        accounts = initAccounts;
+        transactions = initTransactions;
+    }
+
+    public void readFile (String fileName) {
+        String regex = "(?<fileExtension>\\.(json|csv))";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(fileName);
+
+        if (matcher.find()) {
+            if (matcher.group("fileExtension").equals(".json")) {
+                readJsonFile(fileName);
+            } else if (matcher.group("fileExtension").equals(".csv")) {
+                readCsvFile(fileName);
+            }
+        }
+    }
+
+    private void readJsonFile (String fileName) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        try {
+            BufferedReader br = new BufferedReader(new java.io.FileReader(fileName));
+
+            Transaction[] transactionsFromFile = gson.fromJson(br, Transaction[].class);
+            List<Transaction> list = Arrays.asList(transactionsFromFile);
+            for (Transaction transaction : list) {
+
+                transactions.add(transaction);
+                createAccountObjects(accounts, transaction.getFromAccount(), transaction.getToAccount(), transaction.getAmount(), transaction);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("This file does not exist. Please try again.");
+            // e.printStackTrace();
+        }
+    }
+
+    private void readCsvFile (String fileName) {
+        String line = "";
+
+        try {
+            BufferedReader br = new BufferedReader(new java.io.FileReader(fileName));
+
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+
+                if (!values[1].equals("From")) {
+                    String fromAccountName = values[1];
+                    String toAccountName = values[2];
+                    String date = values[0];
+                    String narrative = values[3];
+                    BigDecimal amount = new BigDecimal(values[4]);
+
+                    // create the transaction objects
+                    Transaction theTransaction = new Transaction(date, fromAccountName, toAccountName, narrative, amount);
+                    transactions.add(theTransaction);
+
+                    createAccountObjects(accounts, fromAccountName, toAccountName, amount, theTransaction);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("This file does not exist. Please try again.");
+            // e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createAccountObjects(Map<String, Account> accounts, String fromAccountName, String toAccountName, BigDecimal amount, Transaction theTransaction) {
+        createAccountObject(accounts, fromAccountName, theTransaction);
+        accounts.get(fromAccountName).deductAmount(amount);
+
+        createAccountObject(accounts, toAccountName, theTransaction);
+        accounts.get(toAccountName).addAmount(amount);
+    }
+
+    private static void createAccountObject(Map<String, Account> accounts, String accountName, Transaction theTransaction) {
+        Account account = accounts.get(accountName);
+        if (account == null) {
+            account = new Account(accountName);
+            accounts.put(accountName, account);
+        }
+        accounts.get(accountName).addTransaction(theTransaction);
+    }
+}
--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,17 +1,7 @@
 package training.supportbank;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class Main {
     public static void main(String args[]) {
@@ -22,8 +12,16 @@ public class Main {
         String csv = "Transactions2014.csv";
         String json = "Transactions2013.json";
 
-        readFile(json, accounts, transactions);
-        readFile(csv, accounts, transactions);
+        FileReader fileReader = new FileReader(accounts, transactions);
+//        fileReader.readFile(json);
+//        fileReader.readFile(csv);
+
+        while (accounts.isEmpty()) {
+            Scanner newScan = new Scanner(System.in);
+            System.out.println("Enter the filename you want to read:");
+            String command = newScan.nextLine();
+            fileReader.readFile(command);
+        }
 
         printListByUserInput(accounts);
     }
@@ -44,87 +42,4 @@ public class Main {
         }
     }
 
-    private static void createAccountObject(Map<String, Account> accounts, String accountName, Transaction theTransaction) {
-        Account account = accounts.get(accountName);
-        if (account == null) {
-            account = new Account(accountName);
-            accounts.put(accountName, account);
-        }
-        accounts.get(accountName).addTransaction(theTransaction);
-    }
-
-    private static void readFile (String fileName, Map<String, Account> accounts, ArrayList<Transaction> transactions) {
-        String regex = "(?<fileExtension>\\.(json|csv))";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(fileName);
-
-        if (matcher.find()) {
-            if (matcher.group("fileExtension").equals(".json")) {
-                System.out.println("json");
-                readJsonFile(fileName, accounts, transactions);
-            } else if (matcher.group("fileExtension").equals(".csv")) {
-                System.out.println("csv");
-                readCsvFile(fileName, accounts, transactions);
-            }
-        }
-    }
-
-    private static void readJsonFile (String fileName, Map<String, Account> accounts, ArrayList<Transaction> transactions) {
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-
-            Transaction[] transactionsFromFile = gson.fromJson(br, Transaction[].class);
-            List<Transaction> list = Arrays.asList(transactionsFromFile);
-            for (Transaction transaction : list) {
-
-                transactions.add(transaction);
-                createAccountObjects(accounts, transaction.getFromAccount(), transaction.getToAccount(), transaction.getAmount(), transaction);
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void readCsvFile (String fileName, Map<String, Account> accounts, ArrayList<Transaction> transactions) {
-        String line = "";
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-
-                if (!values[1].equals("From")) {
-                    String fromAccountName = values[1];
-                    String toAccountName = values[2];
-                    String date = values[0];
-                    String narrative = values[3];
-                    BigDecimal amount = new BigDecimal(values[4]);
-
-                    // create the transaction objects
-                    Transaction theTransaction = new Transaction(date, fromAccountName, toAccountName, narrative, amount);
-                    transactions.add(theTransaction);
-
-                    createAccountObjects(accounts, fromAccountName, toAccountName, amount, theTransaction);
-                }
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void createAccountObjects(Map<String, Account> accounts, String fromAccountName, String toAccountName, BigDecimal amount, Transaction theTransaction) {
-        createAccountObject(accounts, fromAccountName, theTransaction);
-        accounts.get(fromAccountName).deductAmount(amount);
-
-        createAccountObject(accounts, toAccountName, theTransaction);
-        accounts.get(toAccountName).addAmount(amount);
-    }
 }
